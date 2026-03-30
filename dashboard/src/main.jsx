f20a09f0b41a93c5c1403b2c3d48d3c512bd185e
Nosher <nosherzapoo@gmail.com>
2026-03-30 09:59:16 -0400

    Add Vercel Analytics

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import LandingPage from './components/LandingPage';
 import ApiAccessPage from './components/ApiAccessPage';
@@ -26,5 +27,6 @@ createRoot(document.getElementById('root')).render(
         <Route path="/app/*" element={<App />} />
       </Routes>
     </BrowserRouter>
+    <Analytics />
   </StrictMode>
 );
